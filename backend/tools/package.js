eab4dbf310dfe232d5e99a0cb6d12b8d90ecf3df
Baris Demiray <baris.demiray@gmail.com>
2020-05-09 14:38:37 +0200

    Start moving code from bad git to good git, here is found version retrieval

--- backend/tools/package.js
+++ backend/tools/package.js
@@ -52,3 +52,74 @@ exports.get_package_entry_point = async function (package_name) {
   console.debug('entry point is', package_entry_point);
   return package_entry_point;
 };
+
+// todo Another way would be 'yarn info', perhaps?
+exports.find_package_dependencies = function (package_name) {
+  // Let's have a closure to keep a cumulative list of dependencies
+  function do_find_package_dependencies(package_name) {
+    try {
+      const package_json_path = require.resolve(`${package_name}/package.json`);
+      console.log('package.json path', package_json_path);
+
+      // Load package.json only if it exists
+      if (fs.existsSync(package_json_path)) {
+        const { version, dependencies } = require(package_json_path);
+
+        dependency_list.push(package_name);
+
+        for (const dep in dependencies) {
+          console.debug(`Found dependency ${dep} of ${package_name}`);
+          dependency_list.push(dep);
+
+          do_find_package_dependencies(dep);
+        }
+      } else {
+        console.warn(`${package_json_path} does not exists, skipping...`);
+      }
+    } catch (error) {
+      console.error('error', error);
+    }
+  }
+
+  // Start the recursive dependency extraction, stack is our friend
+  var dependency_list = [];
+  do_find_package_dependencies(package_name);
+
+  // List is possible not composed of unique elements at this point, make it so
+  dependency_list = [...new Set(dependency_list)];
+
+  return dependency_list;
+};
+
+exports.find_available_versions = async function (package_name) {
+  // We'll get all available versions from 'yarn info'
+  const args = ['info', '--json', package_name];
+
+  try {
+    const { stdout, stderr } = await execa('yarn', args);
+
+    let versions = JSON.parse(stdout).data.versions;
+
+    // Get all major versions
+    const majors = version_tools.get_major_versions(versions);
+
+    // Get last 4 versions, whatever the major is
+    const last_4 = version_tools.get_last_n_versions(versions, 4);
+
+    // Get last 4 versions of the last major version
+    let last_4_of_last_major = [];
+    if (majors) {
+      let [last_major] = majors.slice(-1);
+      last_4_of_last_major = version_tools.get_last_n_versions_of_major(versions, last_major, 4);
+    }
+
+    // Return an object with all required version info
+    return {
+      majors,
+      last_4,
+      last_4_of_last_major,
+    };
+  } catch (error) {
+    console.debug('error', error);
+  }
+};
---
+++ backend/tools/version.js
@@ -0,0 +1,57 @@
+const semver = require('semver');
+
+/**
+ * Cleans invalid or pre-release values from a list with semvers.
+ */
+exports.clean_version_list = function (version_list) {
+  // console.log('received', arguments);
+
+  // Remove all values that is not valid
+  let _versions = version_list.filter((version) => !semver.valid(version_list));
+
+  // Remove all versions that include a prerelease tag, e.g. alpha
+  return _versions.filter((version) => !semver.prerelease(version));
+};
+
+exports.get_major_versions = function (version_list) {
+  // console.log('received', arguments);
+
+  let _versions = this.clean_version_list(version_list);
+
+  let major_versions = [];
+  _versions.map((version) => major_versions.push(semver.major(version)));
+  major_versions = [...new Set(major_versions)];
+
+  return major_versions;
+  // console.log('major_versions', major_versions);
+};
+
+exports.get_last_n_versions = function (version_list, n) {
+  // console.log('received', arguments);
+
+  let _versions = this.clean_version_list(version_list);
+
+  // If there are not enough versions, return the whole list
+  if (_versions.length <= n) {
+    return _versions;
+  }
+
+  return _versions.slice(-n);
+};
+
+exports.get_last_n_versions_of_major = function (version_list, major, n) {
+  console.log('received', arguments);
+
+  let _versions = this.clean_version_list(version_list);
+
+  // Find all versions with given major version
+  _versions = _versions.filter((version) => semver.major(version) === major);
+
+  // If there are not enough versions with given major, return the whole list
+  if (_versions.length <= n) {
+    return _versions;
+  }
+
+  // Else take a slice of it
+  return _versions.slice(-n);
+};
