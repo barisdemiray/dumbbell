8a5c91c743b625ca1bab7a3df85ab25b9a4774bb
Baris Demiray <baris.demiray@gmail.com>
2020-05-09 14:27:32 +0200

    You know what, let's use a same package.json for both backend and frontend. Faster but less neat

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
-"use strict";
+'use strict';
 
-console.log('hello there');
+const package_tools = require('./tools/package');
+
+package_tools.get_package_entry_point('eslint').then((result) => console.log(result));
--- backend/tools/package.js
+++ backend/tools/package.js
@@ -1,6 +1,6 @@
 const execa = require('execa');
-// const path = require('path');
-// const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
 // todo do not bloat our own node_modules, install into temporary directory
 exports.install_package = async function (package_name) {
@@ -27,4 +27,28 @@ exports.uninstall_package = async function (package_name) {
   } catch (error) {
     console.debug('error', error);
   }
+};
+
+exports.get_package_entry_point = async function (package_name) {
+  // First we need the entry point of the package
+  let package_entry_point = '';
+
+  try {
+    // const package_json_path = path.join('./node_modules', package_name, 'package.json');
+    const package_json_path = require.resolve(`${package_name}/package.json`);
+    console.debug('package_json_path', package_json_path);
+
+    // Load package.json only if it exists
+    if (fs.existsSync(package_json_path)) {
+      package_entry_point = require(package_json_path).main;
+    } else {
+      throw new Error(`File ${package_json_path} does not exist`);
+    }
+  } catch (error) {
+    console.error('error', error);
+    throw new Error(`Failed to get entry point for package ${package_name}`);
+  }
+
+  console.debug('entry point is', package_entry_point);
+  return package_entry_point;
 };
