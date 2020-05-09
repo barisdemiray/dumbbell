a92298ab71b6db5db5a8e662279a9a246e8a2a7b
Baris Demiray <baris.demiray@gmail.com>
2020-05-09 22:24:45 +0200

    Better styling for the search box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,42 @@ import SearchBox from './components/searchbox.component';
 function App() {
   // Let's show a feedback while getting data from backend
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
   const [packageName, setPackageName] = useState('');
 
   // todo grab backend response here
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch('http://localhost:8080/?package=' + packageName)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, [packageName]);
 
   const handleChange = (val) => {
     console.debug('search value', val);
+
+    // Package name should not have spaces
+    if (val.indexOf(' ') >= 0) {
+      setError('Package names should not have spaces');
+    } else {
+      setPackageName(val);
+    }
+  };
+
+  const renderError = () => {
+    if (error) {
+      return <span className="SearchError">{error}</span>;
+    }
+
+    return null;
   };
 
   return (
     <div className="App">
-      <div className="SearchBoxContainer">
+      <div className="SearchContainer">
         <SearchBox handleChange={handleChange} />
+        {renderError()}
       </div>
+      <div className="ResultContainer">No results yet</div>
     </div>
   );
 }
--- frontend/src/components/searchbox.component.jsx
+++ frontend/src/components/searchbox.component.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import './searchbox.styles.css';
 
 function SearchBox({ handleChange }) {
+  const [query, setQuery] = useState('');
+
+  /* This is internal, it only updates internal state */
+  const updateChange = (query) => {
+    setQuery(query);
+  };
+
+  /* This is where we elevate the query to parent component */
+  const submitChange = () => {
+    handleChange(query);
+  };
+
   return (
-    <input
-      className="SearchBox"
-      type="text"
-      onChange={(e) => handleChange(e.target.value)}
-      placeholder="Package name, e.g. react"
-    />
+    <div className="SearchBoxContainer">
+      <input
+        id="SearchBox"
+        className="SearchBoxInput"
+        type="text"
+        onChange={(e) => updateChange(e.target.value)}
+        placeholder="Package name, e.g. react"
+      />
+      <img
+        src="./static/images/noun_Search_3179535.svg"
+        className="SearchBoxIcon"
+        onClick={() => submitChange()}
+      />
+    </div>
   );
 }
 
