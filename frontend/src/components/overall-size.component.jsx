aca70d18aea0245c49c77409b714c35712cb846d
Baris Demiray <baris.demiray@gmail.com>
2020-05-10 19:34:53 +0200

    Add a component that renders overall bundle size information

---
+++ frontend/src/components/overall-size.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './overall-size.styles.css';
+
+/**
+ * A component that renders the overall size info with given data.
+ */
+function OverallSize({ data }) {
+  /**
+   * Renders a table with overall size information
+   */
+  console.debug('data in overall size', data);
+  if (data) {
+    const minifiedBundleSizeInKiloBytes = (data.minifiedBundleSizeInBytes / 1024.0).toFixed(2);
+    const minifiedAndGzippedBundleSizeInKiloBytes = (
+      data.minifiedAndGzippedBundleSizeInBytes / 1024
+    ).toFixed(2);
+
+    return (
+      <div className="OverallSizeContainer">
+        <div className="OverallSizeTitle">Bundle size</div>
+        <div className="OverallSize">
+          <div>
+            <span className="OverallSizeValue">{minifiedBundleSizeInKiloBytes}</span>
+            <span className="OverallSizeValueUnit">KiB</span>
+          </div>
+          <span className="OverallSizeDefinition">minified</span>
+        </div>
+        <div className="OverallSize">
+          <div>
+            <span className="OverallSizeValue">{minifiedAndGzippedBundleSizeInKiloBytes}</span>
+            <span className="OverallSizeValueUnit">KiB</span>
+          </div>
+          <span className="OverallSizeDefinition">minified + gzipped</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+// Check props
+OverallSize.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default OverallSize;
--- frontend/src/components/resultbox.component.jsx
+++ frontend/src/components/resultbox.component.jsx
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 
 import './resultbox.styles.css';
 import BarChart from './barchart.component';
+import OverallSize from './overall-size.component';
 
 /**
  * A component that renders package bundle size result.
  */
 function ResultBox({ resultData }) {
+  // Only the most recent version's result data will be displayed in detail
+  const [lastResultData] = resultData.slice(-1);
+
   return (
     <div className="ResultBoxContainer">
+      <OverallSize data={lastResultData} />
       <BarChart data={resultData} />
     </div>
   );
