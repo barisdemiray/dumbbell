93377e9063b0c84ccd23aea416b76f4c66423c70
Baris Demiray <baris.demiray@gmail.com>
2020-05-10 10:53:05 +0200

    Comment and cleanup package name validation

--- frontend/src/tools/package.js
+++ frontend/src/tools/package.js
@@ -1,21 +1,28 @@
+/**
+ * Checks if given NPM package name is valid.
+ *
+ * @todo More rules to be added from https://docs.npmjs.com/files/package.json.
+ * @param {String} Package name to verify.
+ * @return {Object} An object containing a boolean and a reason field. Reason will be empty if result is true.
+ */
 export const isPackageNameValid = (package_name) => {
-  // todo https://docs.npmjs.com/files/package.json
-
+  // Start with an invalid state
   let result = {
     valid: false,
-    message: '',
+    reason: '',
   };
 
   if (typeof package_name !== 'string') {
-    result.message = 'Package name has to be string';
+    result.reason = 'Package name has to be string';
   } else if (package_name.indexOf(' ') >= 0) {
-    result.message = 'Package names should not have spaces';
+    result.reason = 'Package names should not have spaces';
   } else if (package_name.length === 0) {
-    result.message = 'Package name cannot be empty';
+    result.reason = 'Package name cannot be empty';
   } else if (package_name.charAt(0) === '.' || package_name.charAt(0) === '_') {
-    result.message = 'Package names cannot start with dot or underscore characters';
+    result.reason = 'Package names cannot start with a dot or an underscore';
   } else {
-    result.message = '';
+    result.valid = true;
+    result.reason = '';
   }
 
   return result;
