335548b7f3ec583541939f54eec0ee60dc550a81
Baris Demiray <baris.demiray@gmail.com>
2020-05-10 11:16:50 +0200

    Adopt AirBnb styling for comments too

--- frontend/src/components/searchbox.component.jsx
+++ frontend/src/components/searchbox.component.jsx
@@ -1,23 +1,28 @@
 import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
+
 import './searchbox.styles.css';
 
+/**
+ * A component that displays a package search box. Keeps an internal state as well.
+ */
 function SearchBox({ handleChange, handleSubmit }) {
+  // Internal state for the user-provided query
   const [query, setQuery] = useState('');
 
-  /* This is internal, it only updates internal state */
+  // This is internal, it only updates internal state
   const updateChange = (query) => {
     setQuery(query);
     // Also let parent component know for possible error messages
     handleChange(query);
   };
 
-  /* This is where we elevate the query to parent component */
+  // This is where we elevate the query to parent component
   const submitChange = () => {
     handleSubmit(query);
   };
 
-  /* Better user experience by handling enter key on the search box */
+  // Better user experience by handling enter key on the search box
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       handleSubmit(query);
